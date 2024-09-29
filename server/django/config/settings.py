402d8fab1c7ead97d1440be1814f637b5ad8f129
harune-pg <h.makino.22.6.533@4nonome.com>
2024-09-29 13:36:29 +0900

    fix s3 settings

--- server/django/config/settings.py
+++ server/django/config/settings.py
@@ -139,17 +139,20 @@ if USE_S3_FOR_STATIC:
     STORAGES = {
         "default": {
             "BACKEND": "storages.backends.s3.S3Storage",
+            "location": "media",
         },
         "staticfiles": {
             "BACKEND": "storages.backends.s3.S3Storage",
+            "location": "static",
         },
     }
 
     AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
-    AWS_S3_BUCKET_NAME = env.str("AWS_S3_BUCKET_NAME")
+    AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME")
 
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_S3_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
