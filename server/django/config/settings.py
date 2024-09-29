f9f4c5b8c0953861d2dcd3f643eea10955ae7d4f
harune-pg <h.makino.22.6.533@4nonome.com>
2024-09-29 12:45:48 +0900

    change os.env to env.str

--- server/django/config/settings.py
+++ server/django/config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 """
 
-import os
 from pathlib import Path
 
 import environ
@@ -146,9 +145,9 @@ if USE_S3_FOR_STATIC:
         },
     }
 
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
+    AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_BUCKET_NAME = env.str("AWS_S3_BUCKET_NAME")
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_S3_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
