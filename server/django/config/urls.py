b60a4854a10cb594007715a6b913d25fef4d7616
harune-pg <makkino7086@gmail.com>
2024-12-11 11:35:19 +0900

    apply lint

--- server/django/config/urls.py
+++ server/django/config/urls.py
@@ -3,10 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = (
-    [
-        path("admin/", admin.site.urls),
-        path("memos/", include("memos.urls")),
-    ]
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("memos/", include("memos.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
